45ff11a93a0779d8030f2629d8b549418a64d18d
Rardawn21 <39149628+Rardawn21@users.noreply.github.com>
2022-04-07 21:46:58 +0300

    Update file.lua

--- file.lua
+++ file.lua
@@ -1,8 +1,2 @@
-local name = ""  -- username list type: sex1(Admin), sex2, sex3
-if string.find(name, .."(Admin)") then
-  return 2
-elseif string.find(name, ) then
-  return 1
-else
-  return 0
-end
+local hwids = ""
+return hwid
